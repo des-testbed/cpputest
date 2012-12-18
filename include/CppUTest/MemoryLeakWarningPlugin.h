f9e94e88d514c800d6b5c74316651fe5a3ba693f
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-18 19:00:37 +0100

    Commented two lines to make the memory leak detection work
    The source code of my projects that used CppUTest and the MemoryLeak
    Headers do not compile if i uncomment the lines :( I will look into this
    problem later.

--- include/CppUTest/MemoryLeakWarningPlugin.h
+++ include/CppUTest/MemoryLeakWarningPlugin.h
@@ -49,8 +49,8 @@
 #if CPPUTEST_USE_STD_CPP_LIB
 
 #include <new>
-void* operator new(size_t size) throw(std::bad_alloc);
-void* operator new[](size_t size) throw(std::bad_alloc);
+//void* operator new(size_t size) throw(std::bad_alloc);
+//void* operator new[](size_t size) throw(std::bad_alloc);
 void* operator new(size_t size, const std::nothrow_t&) throw();
 void* operator new[](size_t size, const std::nothrow_t&) throw();
 void operator delete(void* mem) throw();
